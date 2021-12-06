82cecb07d44d9957b29acb02e92028b3a97c6b72
Travis Dixon <travis@growdata.com.au>
2021-12-06 16:02:03 +1100

    day 2

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -1,5 +1,46 @@
+import java.io.BufferedReader
+
+const val forward = "forward "
+const val up = "up "
+const val down = "down "
+
 class day2 {
     fun run () {
+        val input = this::class.java.classLoader.getResource("input_2.txt").openStream().bufferedReader()
+        print("position: ${positionWithAim(input)}\n")
+    }
 
+    fun testAndApply(line: String, prefix: String, fn: (Int) -> Unit): Boolean {
+        if(line.startsWith(prefix)) {
+            fn(Integer.parseInt(line.substringAfter(prefix)))
+            return true
+        }
+        return false
+    }
+
+    fun position(input: BufferedReader): Int {
+        var depth = 0
+        var horizontal = 0
+        input.forEachLine {
+            testAndApply(it, forward, {horizontal += it}) ||
+            testAndApply(it, up, {depth -= it}) ||
+            testAndApply(it, down, {depth += it})
+        }
+        return depth * horizontal
+    }
+
+    fun positionWithAim(input: BufferedReader): Int {
+        var depth = 0
+        var aim = 0
+        var horizontal = 0
+        input.forEachLine {
+            testAndApply(it, forward, {
+                horizontal += it
+                depth += aim * it
+            }) ||
+                    testAndApply(it, up, {aim -= it}) ||
+                    testAndApply(it, down, {aim += it})
+        }
+        return depth * horizontal
     }
 }
